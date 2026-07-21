c5f0d15ec2383cea0ff4547e6a171cbe8e6760ed
Didikoral1 <164909424+Didikoral1@users.noreply.github.com>
2026-07-21 23:06:29 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { news } from "@/data/news";
-import HomeClient from "@/components/HomeClient";
-
-export const dynamic = "force-dynamic";
-
-export default function Home() {
-  return <HomeClient news={news} />;
-}
